ee7d78c144bd1a0e10b76f2d56e894e2319121aa
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2025-11-26 14:20:50 +0200

    Created the Modal component class with associated funcionality

---
+++ client/src/components/Modal.js
@@ -0,0 +1,24 @@
+class Modal {
+  constructor() {
+    this._modal = document.getElementById("modal");
+    this._modalBtn = document.getElementById("modal-btn");
+    this._modalBtn.addEventListener("click", this.open.bind(this));
+    window.addEventListener("click", this.outsideClick.bind(this));
+  }
+
+  open() {
+    this._modal.style.display = "block";
+  }
+
+  close() {
+    this._modal.style.display = "none";
+  }
+
+  outsideClick(e) {
+    if (e.target === this._modal) {
+      this.close();
+    }
+  }
+}
+
+export default Modal;
